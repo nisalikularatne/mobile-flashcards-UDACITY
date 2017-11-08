854466fdd8da1fdc61ec4b552dc5a5146f89a000
Nisali Kularatne <nisalikularatne@yahoo.com>
2017-11-08 16:08:37 +0800

    DeckList component added

---
+++ components/DeckList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
+export default class DeckList extends Component {
+render() {
+
+    return (
+        <View>
+            <Text>DeckList</Text>
+        </View>
+
+    )
+}
+}

f2d9773630f53a1c8231b220acc202d50dae1bf6
Nisali Kularatne <nisalikularatne@yahoo.com>
2017-11-08 17:08:21 +0800

    Testing the DeckList component with sample data and rendering it using FlatList

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react'
-import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native'
-export default class DeckList extends Component {
-render() {
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-    return (
-        <View>
-            <Text>DeckList</Text>
-        </View>
+     const state= [
+        {
+            id: 0,
+            title: 'React and Redux Minutes',
+            cards: [1, 2, 3, 4]
+    },
 
-    )
-}
-}
+
+];
+class DeckList extends React.Component {
+            renderItem = ({item}) => {
+                    return (
+                            <View >
+                                    <Text >{item.title}</Text>
+                                    <Text >{item.cards.length}</Text>
+                                </View>
+                        );
+                };
+
+            render() {
+                    return (
+                            <FlatList data={state} renderItem={this.renderItem} />
+                        );
+                }
+    }
+
+
+export default DeckList
